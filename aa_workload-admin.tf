5f453bcdb7357c4687be4e5886f50ecf2dd7d511
bigfantech <130840188+bigfantech@users.noreply.github.com>
2023-05-09 16:31:37 +0530

    Change: group ref method

--- aa_workload-admin.tf
+++ aa_workload-admin.tf
@@ -1,9 +1,9 @@
 resource "aws_ssoadmin_account_assignment" "workloadadmins" {
-  for_each = toset(lookup(local.group_name_to_association_accounts, "WorkloadAdmins", null))
+  for_each = local.group_name_to_association_accounts["WorkloadAdmins"]
 
   instance_arn       = local.identity_store_instance_arn
   permission_set_arn = aws_ssoadmin_permission_set.administratoraccess_custom.arn
-  principal_id       = lookup(local.identity_centre_group_display_name_to_group_id_map, "WorkloadAdmins", null)
+  principal_id       = local.identity_centre_group_display_name_to_group_id_map["each.key"]
   principal_type     = "GROUP"
   target_id          = each.value
   target_type        = "AWS_ACCOUNT"
