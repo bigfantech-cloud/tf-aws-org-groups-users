526a3721e59eb6c14bca35d941fec799c082779b
bigfantech <130840188+bigfantech@users.noreply.github.com>
2023-05-09 18:13:38 +0530

    Misc

--- aa_workload-admin.tf
+++ aa_workload-admin.tf
@@ -3,7 +3,7 @@ resource "aws_ssoadmin_account_assignment" "workloadadmins" {
 
   instance_arn       = local.identity_store_instance_arn
   permission_set_arn = aws_ssoadmin_permission_set.administratoraccess_custom.arn
-  principal_id       = local.identity_centre_group_display_name_to_group_id_map["each.key"]
+  principal_id       = local.identity_centre_group_display_name_to_group_id_map["WorkloadAdmins"]
   principal_type     = "GROUP"
   target_id          = each.value
   target_type        = "AWS_ACCOUNT"
