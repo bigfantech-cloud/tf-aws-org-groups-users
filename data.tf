07c1525edadfd915c6a11d0cb394944c073c7ef4
bigfantech <130840188+bigfantech@users.noreply.github.com>
2023-05-09 16:26:53 +0530

    Misc

--- data.tf
+++ data.tf
@@ -6,11 +6,12 @@ locals {
 
   users_config                                       = { for map in var.users_config : map.email_id => map }
   groups_config                                      = { for map in var.groups_config : map.display_name => map }
-  group_name_to_association_accounts                 = { for map in var.groups_config : map.display_name => lookup(map, "aws_accounts_association", []) }
+  group_name_to_association_accounts                 = { for map in var.groups_config : map.display_name => map["aws_accounts_association"] }
   identity_centre_user_name_to_user_id_map           = { for k, user in aws_identitystore_user.all : aws_identitystore_user.all[k].user_name => aws_identitystore_user.all[k].user_id }
   identity_centre_group_display_name_to_group_id_map = { for k, group in aws_identitystore_group.all : aws_identitystore_group.all[k].display_name => aws_identitystore_group.all[k].group_id }
 
   permission_set_prefix = "sso_permissionset"
+    
   office_address = {
     head_office_address = [{
       formatted      = "TN, India"
