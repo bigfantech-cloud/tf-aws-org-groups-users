0362c72ab25f60b07e3685d0988f1c4134b50cbc
bigfantech <130840188+bigfantech@users.noreply.github.com>
2023-05-09 16:18:42 +0530

    Change: value interpolation type change

--- vpn-groups.tf
+++ vpn-groups.tf
@@ -2,7 +2,7 @@ resource "aws_identitystore_group" "vpn_all" {
   for_each = local.vpn_groups_config
 
   display_name      = each.value["display_name"]
-  description       = "bigfantech Org VPN access group. Application - SG VPN Authenticator"
+  description       = "VPN access group. Application - VPN Authenticator"
   identity_store_id = local.identity_store_id
 }
 
@@ -10,8 +10,8 @@ resource "aws_identitystore_group_membership" "vpn_all" {
   for_each = local.vpn_groups_to_users_map
 
   identity_store_id = local.identity_store_id
-  group_id          = lookup(local.vpn_identity_centre_group_display_name_to_group_id_map, each.value.group_name, null)
-  member_id         = lookup(local.identity_centre_user_name_to_user_id_map, each.value.user_name, null)
+  group_id          = local.vpn_identity_centre_group_display_name_to_group_id_map[each.value.group_name]
+  member_id         = local.identity_centre_user_name_to_user_id_map[each.value.user_name]
 
   depends_on = [
     aws_identitystore_group.vpn_all
